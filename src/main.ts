875d281dbf5133d62fa5fc2eef8a90187d049189
Basile Antinomy studio <basile@antinomy.studio>
2025-10-28 10:28:58 +0100

    feat: test 12

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 export function clamp(val: number, min: number, max: number): number {
-  console.log('nothing more')
+  console.log('nothing more test')
   return Math.max(Math.min(val, max), min)
 }
